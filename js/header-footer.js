f2a26909bb30df32a89078cf77bb40a6773b654e
gokulraj <rajgokul.v2000@gamil.com>
2022-12-03 18:35:57 +0530

    Add productlist page

--- js/header-footer.js
+++ js/header-footer.js
@@ -1,26 +1,31 @@
 $(document).ready(function () {
-fetch('/header.html')
-.then(response => response.text())
-.then(function (headerHtml) {
-$('#main-header').html(headerHtml);
-});
-fetch('/footer.html')
-.then(response => response.text())
-.then(function (footerHtml) {
-$('#main-footer').html(footerHtml);
-fetch('/product-view.html')
-.then(response => response.text())
-.then(function (productViewHtml) {
-$('#main-container').html(productViewHtml);
-});
-});
-});
+	fetch('/header.html')
+		.then((response) => response.text())
+		.then(function (headerHtml) {
+			$('#main-header').html(headerHtml)
+		})
+	fetch('/footer.html')
+		.then((response) => response.text())
+		.then(function (footerHtml) {
+			$('#main-footer').html(footerHtml)
+			fetch('/productList.html')
+				.then((response) => response.text())
+				.then(function (homePageHtml) {
+					$('#main-container').html(homePageHtml)
+				})
+		})
+})
 
-$(document).on("click","#register",function() {
-  fetch('/profile.html')
-  .then(response => response.text())
-  .then(function (profileHtml) {
-  $('#main-container').html(profileHtml);
-  });
-});
+$(document).on('click', '#register', function () {
+	fetch('/login.html')
+		.then((response) => response.text())
+		.then(function (loginHtml) {
+			$('#main-container').html(loginHtml)
+		})
+})
 
+// fetch('/homePage.html')
+// 	.then((response) => response.text())
+// 	.then(function (homePageHtml) {
+// 		$('#main-container').html(homePageHtml)
+// 	})
